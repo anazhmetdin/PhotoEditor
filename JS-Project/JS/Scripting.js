7d6b0f3978a0ff82b479562db54abf6cf749b7bb
anazhmetdin <anazhmetdin@gmail.com>
2022-12-28 17:24:13 +0200

    fix buttons styling

--- JS-Project/JS/Scripting.js
+++ JS-Project/JS/Scripting.js
@@ -2,23 +2,24 @@ var currentSection = 0;
 
 $(function(){
     $(".drawing-board,.image-board,.text-board").hide();
+    $('input[name=menu]').prop('checked',false)
 
     $("#btnDraw").click(function(){
         currentSection = 1;
         $(".image-board,.text-board").hide();
-        $(".drawing-board").show("fast");
+        $(".drawing-board").show();
     })
 
     $("#btnEdit").click(function(){
         currentSection = 2;
         $(".drawing-board,.text-board").hide();
-        $(".image-board").show("fast");
+        $(".image-board").show();
     })
 
     $("#btnText").click(function(){
         currentSection = 3;
         $(".drawing-board,.image-board").hide();
-        $(".text-board").show("fast");
+        $(".text-board").show();
     })
 
 })
